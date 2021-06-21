2e0fd8e574bb621e5de51673cca298f2157397b3
lukeseeley <luke@seeleyone.com>
2021-06-21 15:31:18 -0600

    Added Card Components
    I have added Card Components to the tracking index page. I also touched up on some of the styling.

--- ServerProjectTracker/ServerProjectTracker/Pages/Index.cshtml.cs
+++ ServerProjectTracker/ServerProjectTracker/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,11 +12,19 @@ namespace ServerProjectTracker.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly Data.ServerProjectTrackerContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, Data.ServerProjectTrackerContext context)
         {
             _logger = logger;
+            _context = context;
         }
+
+        [BindProperty, Required]
+        public string Username { get; set; }
+
+        [BindProperty, Required]
+        public string Password { get; set; }
 
         public void OnGet()
         {
