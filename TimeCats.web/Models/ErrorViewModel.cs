609603071b8d9e4c5d8691a748ad4cd94fcf72a8
Ben-Mullins <48454984+Ben-Mullins@users.noreply.github.com>
2021-08-07 11:34:46 -0600

    Mass Deletion
    Please make sure I didn't delete anything that should not have been deleted before merging.

--- TimeCats.web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TimeCats.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
